d96faa8e96fe1941eb9c3b3280434650defce0ff
HP <rejoicemawusi57@gmail.com>
2024-06-26 10:06:44 -0700

    adding index.html src /

---
+++ src/index.js
@@ -0,0 +1,7 @@
+let name= prompt("What is your name?");
+let age= prompt("How old are you?");
+let email= prompt("your email");
+alert(`Hi ${name},you are ${age} years old and we will keep in touch via your email "${email}"`);
+
+git add README.md
+git add README.md
